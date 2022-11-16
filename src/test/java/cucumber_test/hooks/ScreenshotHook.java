0222d1a66db3e4397fb38906f7a47cecf0551b72
Eugene Kuzhelnyj <kuzhelnyj@tut.by>
2022-11-16 22:40:41 +0300

    fix some mistakes

--- src/test/java/cucumber_test/hooks/ScreenshotHook.java
+++ src/test/java/cucumber_test/hooks/ScreenshotHook.java
@@ -1,14 +1,13 @@
 package cucumber_test.hooks;
 
 import com.codeborne.selenide.Selenide;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.Scenario;
+import org.junit.jupiter.api.AfterEach;
 import org.openqa.selenium.OutputType;
 
 public class ScreenshotHook {
     public static final String PNG_FILE_EXTENSION = "image/png";
-
-@AfterAll
+@AfterEach
     public void takeScreenshot(Scenario scenario){
         byte[] screenshot = Selenide.screenshot(OutputType.BYTES);
         scenario.attach(screenshot,PNG_FILE_EXTENSION,scenario.getName());
--- src/test/java/cucumber_test/runner/CucumberRunnerTest.java
+++ src/test/java/cucumber_test/runner/CucumberRunnerTest.java
@@ -1,9 +1,12 @@
 package cucumber_test.runner;
 
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasspathResource("src/test/java/resources/cucumber_test")
+@SelectClasspathResource("cucumber_test")
+@IncludeEngines("cucumber")
 public class CucumberRunnerTest {
+
 }
